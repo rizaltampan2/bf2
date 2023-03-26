60097fe987da81eea4e9daff1495806c880d2368
rizaltampan2 <127279176+rizaltampan2@users.noreply.github.com>
2023-03-26 13:01:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,75 +1,48 @@
 const ethers = require("ethers");
-
 const Web3   = require("web3");
 
-const bip39  = require("bip39");
+const mnemonicWords = ['apple', 'banana', 'cat', 'dog', 'elephant', 'fish', 'grape', 'horse', 'iguana', 'jaguar'];
 
 const api = 'https://eth-mainnet.g.alchemy.com/v2/oyHE_UtB3MYCGrZBaQNXzD1JDZMycYLf';
-
 const provider = new Web3(new Web3.providers.HttpProvider(api));
 
 async function main() {
-
   while(1){
-
-    var mnemonic = bip39.generateMnemonic();
-
-    var wallet   = ethers.Wallet.fromMnemonic(mnemonic);
-
-    var address  = wallet.address;    
-
+    var mnemonic = generateMnemonic();
+    var wallet = ethers.Wallet.fromMnemonic(mnemonic);
+    var address = wallet.address;
     var balance = await provider.eth.getBalance(address);
-
     
-
-    if (balance !== '0')// eth in this account
-
-    {
-
+    if (balance !== '0'){ // eth in this account
       // write mnemonic and address to cracked.txt
-
-      const fs = require('fs')
-
-      const content = mnemonic+'\n'+address+'\n'
-
-      
-
+      const fs = require('fs');
+      const content = mnemonic+'\n'+address+'\n';
       fs.appendFile('cracked.txt', content, err => {
-
         if (err) {
-
-          console.error(err)
-
+          console.error(err);
           return;
-
         }
-
-      })
-
+      });
     }  
-
-    //    
-
     console.log(address);
-
     console.log("balance: ", balance);
-
   }
+}
 
+function generateMnemonic(){
+  let mnemonic = '';
+  for(let i=0;i<12;i++){
+    let index = Math.floor(Math.random() * mnemonicWords.length);
+    mnemonic += mnemonicWords[index] + ' ';
+  }
+  return mnemonic.trim();
 }
 
 main()
-
-    .then(() => {
-
-        process.exit(0)
-
-    })
-
-    .catch((error) => {
-
-        console.error(error);
-
-        process.exit(1);
-
-    })
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
